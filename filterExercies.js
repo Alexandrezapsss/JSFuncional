df00a40f0da1cc5d27b99528bc35f7a5a07b802b
Alexandrejfox <alexandre.queiroz@jfox.com.br>
2024-09-22 20:51:19 -0300

    mais exercicios de filter

--- filterExercies.js
+++ filterExercies.js
@@ -9,3 +9,23 @@ const numerosPares = numeros.filter(num => num % 2 == 0);
 for(num = 0; num < numerosPares.length; num ++){
     console.log(numerosPares[num]);
 }
+
+//dado uma lista de plavras pegue para uma nova ista as palabras com menos de 5 caracteres
+const palavras = ['bola', 'arvore', 'caminhão', 'carro', 'você', 'estado', 'bacia'];
+
+const menosDe5Letras = palavras.filter(palavra => palavra.length <= 5);
+
+console.log(menosDe5Letras);
+
+//Dada uma lista de objetos representando pessoas, filtre apenas as pessoas com idade maior que 18.
+
+const pessoas = [
+    {nome : 'Alexandre', idade : 30},
+    {nome : 'Beatriz', idade : 20},
+    {nome : 'Edileuza', idade : 17},
+    {nome : 'Laedio', idade : 16},
+    {nome : 'Scooby', idade : 11}
+];
+
+const menoresDeIdade = pessoas.filter(pessoa => pessoa.idade < 18);
+console.log(menoresDeIdade);
