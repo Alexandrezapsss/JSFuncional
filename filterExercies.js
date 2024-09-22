4ab21cd4fb72b188b06fe171f1c048730a80522d
Alexandrejfox <alexandre.queiroz@jfox.com.br>
2024-09-22 15:08:05 -0300

    Exercicios com filter 1

--- filterExercies.js
+++ filterExercies.js
@@ -1,1 +1,11 @@
 //exercicios com filter em JS
+//filter em js filtra elementos de uma lista e o transforma em uma nova lista de valores com base no criterio do filtro
+
+//ex filtre somente os números pares de uma lista
+const numeros = [0,5,8,4,7,6,14,18,29];
+
+const numerosPares = numeros.filter(num => num % 2 == 0);
+
+for(num = 0; num < numerosPares.length; num ++){
+    console.log(numerosPares[num]);
+}
