931c5344379210de1e994b9a1c45e1c1457075c8
Alexandrejfox <alexandre.queiroz@jfox.com.br>
2024-09-24 20:00:49 -0300

    Orientacao a Objeto básico

---
+++ oo_1.js
@@ -0,0 +1,20 @@
+function Produto(nome, preco) {
+    // this é necessário para atribuir ao objeto em sua criação
+    this.nome = nome;
+    this.preco = preco;
+    
+    // Podemos criar valores e atributos privados visíveis somente dentro da função
+    let atributoPrivado = 'Privado para manipulação mas posso ser exibido dentro de uma função construída dentro do obj';
+    let desconto = 0.15;
+
+    this.showPrivateAttribute = () => console.log(atributoPrivado);
+
+    this.precoTotal = () => this.preco * (1 - desconto);
+}
+
+// Instanciando objetos
+const p1 = new Produto('TV', 2500);
+console.log(p1.nome); // TV
+console.log(p1.precoTotal()); // 2125
+
+p1.showPrivateAttribute(); // Privado para manipulação mas posso ser exibido dentro de uma função construída dentro do obj
